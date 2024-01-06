0600d3eaa58c1d01f8f8324474fd3eb3494051f9
okooo5km <okooo5km@gmail.com>
2024-01-06 17:46:01 +0800

    ✨ feat(comment): 表格和视频支持评论

--- components/portable-text/PortableTextVideo.tsx
+++ components/portable-text/PortableTextVideo.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { type PortableTextComponentProps } from '@portabletext/react'
+import * as Dialog from '@radix-ui/react-dialog'
 import { Card } from '@sanity/ui'
+import { AnimatePresence, motion } from 'framer-motion'
 import React from 'react'
 
 import { ClientOnly } from '~/components/ClientOnly'
+import { Commentable } from '~/components/Commentable'
 
 export function PortableTextVideo({
   value,
@@ -13,19 +16,71 @@ export function PortableTextVideo({
   url: string | undefined
   title: string
 }>) {
+  const [isZoomed, setIsZoomed] = React.useState(false)
   if (!value.url) {
     return <Card padding={4}>Missing Video URL</Card>
   }
 
   return (
-    <ClientOnly>
-      {typeof value.url === 'string' && (
-        <div className="flex justify-center">
-          <video className="rounded-xl md:rounded-3xl" controls>
-            <source src={value.url} type="video/mp4" />
-          </video>
-        </div>
-      )}
-    </ClientOnly>
+    <div data-blockid={value._key} className="group relative pr-3 md:pr-0">
+      <ClientOnly>
+        <Commentable className="z-30" blockId={value._key} />
+      </ClientOnly>
+
+      <Dialog.Root open={isZoomed} onOpenChange={setIsZoomed}>
+        <AnimatePresence>
+          {!isZoomed && (
+            <div>
+              <motion.div className="relative" layoutId={`image_${value._key}`}>
+                <Dialog.Trigger>
+                  <video className="rounded-xl md:rounded-3xl" controls>
+                    <source src={value.url} type="video/mp4" />
+                  </video>
+                </Dialog.Trigger>
+              </motion.div>
+            </div>
+          )}
+        </AnimatePresence>
+
+        <AnimatePresence>
+          {isZoomed && (
+            <Dialog.Portal forceMount>
+              <Dialog.Close className="fixed inset-0 z-50 flex h-screen w-screen cursor-zoom-out items-center justify-center">
+                {/* Overlay */}
+                <Dialog.Overlay asChild>
+                  <motion.div
+                    className="absolute inset-0 bg-black/50"
+                    initial={{ opacity: 0, backdropFilter: 'blur(0)' }}
+                    animate={{ opacity: 1, backdropFilter: 'blur(12px)' }}
+                    exit={{ opacity: 0, backdropFilter: 'blur(0)' }}
+                  />
+                </Dialog.Overlay>
+
+                <Dialog.Content className="w-full overflow-hidden">
+                  <div className="relative flex aspect-[3/2] items-center justify-center">
+                    <div className="relative flex aspect-[3/2] w-full max-w-7xl items-center">
+                      <motion.div
+                        layoutId={`image_${value._key}`}
+                        className="absolute inset-0"
+                        transition={{
+                          type: 'spring',
+                          stiffness: 300,
+                          damping: 30,
+                          duration: 0.5,
+                        }}
+                      >
+                        <video className="rounded-xl md:rounded-3xl" controls>
+                          <source src={value.url} type="video/mp4" />
+                        </video>
+                      </motion.div>
+                    </div>
+                  </div>
+                </Dialog.Content>
+              </Dialog.Close>
+            </Dialog.Portal>
+          )}
+        </AnimatePresence>
+      </Dialog.Root>
+    </div>
   )
 }
